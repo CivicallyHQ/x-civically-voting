47795a6511fd8206c0c39600e6ca745955445a87
Angus McLeod <angus@mcleod.org.au>
2018-01-16 11:49:18 +0800

    init

---
+++ assets/javascripts/discourse/initializers/vote-edits.js.es6
@@ -0,0 +1,183 @@
+import { withPluginApi } from 'discourse/lib/plugin-api';
+import Category from 'discourse/models/category';
+import { ajax } from 'discourse/lib/ajax';
+import RawHtml from 'discourse/widgets/raw-html';
+import { popupAjaxError } from 'discourse/lib/ajax-error';
+import showModal from 'discourse/lib/show-modal';
+import { h } from 'virtual-dom';
+
+export default {
+  name: 'vote-edits',
+  initialize() {
+    withPluginApi('0.8.11', api => {
+
+      api.reopenWidget('vote-box', {
+        html(attrs, state){
+          var voteCount = this.attach('vote-count', attrs);
+          var voteButton = this.attach('vote-button', attrs);
+          var voteOptions = this.attach('vote-options', attrs);
+          let contents = [voteCount, voteButton, voteOptions];
+
+          if (state.votesAlert > 0) {
+            let text = "voting.votes_left";
+            let textParams = {
+              count: state.votesAlert,
+              path: this.currentUser.get("path") + "/activity/votes",
+            };
+
+            if (attrs.category.has_vote_limit) {
+              text = "voting.votes_left_category";
+              textParams['categoryName'] = attrs.category.name;
+            }
+
+            const html = "<div class='voting-popup-menu vote-options popup-menu'>" + I18n.t(text, textParams) + "</div>";
+            contents.push(new RawHtml({html}));
+          }
+
+          return contents;
+
+        },
+
+        addVote(){
+          var topic = this.attrs;
+          var state = this.state;
+          return ajax("/voting/vote", {
+            type: 'POST',
+            data: {
+              topic_id: topic.id
+            }
+          }).then(result => {
+            this.updateTopic(result);
+            this.updateCategory(result);
+            this.updateUser(result);
+
+            if (result.alert) {
+              state.votesAlert = result.votes_left;
+            }
+
+            state.allowClick = true;
+            this.scheduleRerender();
+          }).catch(popupAjaxError);
+        },
+
+        removeVote(){
+          const topic = this.attrs;
+          const state = this.state;
+
+          return ajax("/voting/unvote", {
+            type: 'POST',
+            data: {
+              topic_id: topic.id
+            }
+          }).then(result => {
+            this.updateTopic(result);
+            this.updateCategory(result);
+            this.updateUser(result);
+
+            state.allowClick = true;
+            this.scheduleRerender();
+          }).catch(popupAjaxError);
+        },
+
+        updateCategory(result) {
+          const categoryId = this.attrs.category_id;
+          const category = Category.findById(categoryId);
+
+          if (result.hasOwnProperty('category_votes_exceeded')) {
+            category.set('votes_exceeded', result.category_votes_exceeded);
+          }
+        },
+
+        updateTopic(result) {
+          const topic = this.attrs;
+          topic.set('vote_count', result.vote_count);
+          topic.set('user_voted', result.user_voted);
+          topic.set('who_voted', result.who_voted);
+        },
+
+        updateUser(result) {
+          this.currentUser.set('votes_exceeded', result.user_votes_exceeded);
+        }
+      });
+
+      api.reopenWidget('vote-button', {
+        html(attrs){
+          var buttonTitle = I18n.t('voting.vote_title');
+          const user = this.currentUser;
+
+          if (!user){
+            buttonTitle = I18n.t('log_in');
+          }
+          else{
+            if (attrs.closed){
+              buttonTitle = I18n.t('voting.voting_closed_title');
+            }
+            else{
+              if (attrs.user_voted){
+                buttonTitle = I18n.t('voting.voted_title');
+              }
+              else{
+                if (user && this.votesExceeded()){
+                  buttonTitle = I18n.t(`voting.voting_limit`);
+                }
+                else{
+                  buttonTitle = I18n.t('voting.vote_title');
+                }
+              }
+            }
+          }
+          return buttonTitle;
+        },
+
+        click(){
+          if (!this.currentUser){
+            showModal('login');
+          }
+
+          const votesExceeded = this.votesExceeded();
+
+          if (!this.attrs.closed && !votesExceeded && this.parentWidget.state.allowClick && !this.attrs.user_voted){
+            this.parentWidget.state.allowClick = false;
+            this.parentWidget.state.initialVote = true;
+            this.sendWidgetAction('addVote');
+          }
+
+          if (this.attrs.user_voted || votesExceeded) {
+            $(".vote-options").toggle();
+          }
+        },
+
+        votesExceeded() {
+          const category = this.attrs.category;
+          const user = this.currentUser;
+          return category.has_vote_limit ? category.votes_exceeded : user && user.votes_exceeded;
+        }
+      });
+
+      api.reopenWidget('vote-options', {
+        html(attrs){
+          var contents = [];
+
+          if (attrs.user_voted){
+              contents.push(this.attach('remove-vote', attrs));
+          }
+          else if (this.currentUser && !attrs.user_voted && (attrs.category.votes_exceeded || this.currentUser.votes_exceeded)) {
+            let text = I18n.t('voting.reached_limit');
+
+            if (attrs.category.votes_exceeded) {
+              text = I18n.t('voting.reached_category_limit', { categoryName: attrs.category.name });
+            }
+
+            contents.push([
+                h("div", text),
+                h("p",
+                  h("a",{ href: this.currentUser.get("path") + "/activity/votes" }, I18n.t("voting.list_votes"))
+                )
+            ]);
+          }
+          return contents;
+        }
+      });
+    });
+  }
+};
---
+++ plugin.rb
@@ -0,0 +1,203 @@
+# name: civically-voting-extension
+# about: Extends the voting plugin to have category specific limits
+# version: 0.1
+# authors: Angus McLeod
+# url: https://github.com/civicallyhq/x-civically-voting
+
+register_asset 'stylesheets/mobile/voting-extension.scss', :mobile
+
+after_initialize do
+  Category.register_custom_field_type('apply_site_vote_limits', :boolean)
+  Category.register_custom_field_type('tl0_vote_limit', :integer)
+  Category.register_custom_field_type('tl1_vote_limit', :integer)
+  Category.register_custom_field_type('tl2_vote_limit', :integer)
+  Category.register_custom_field_type('tl3_vote_limit', :integer)
+  Category.register_custom_field_type('tl4_vote_limit', :integer)
+
+  require_dependency 'basic_category_serializer'
+  class ::BasicCategorySerializer
+    attributes :has_vote_limit, :votes_exceeded, :site_vote_limits
+
+    def has_vote_limit
+      scope && scope.user && scope.user.has_category_limit?(object.id)
+    end
+
+    def votes_exceeded
+      scope.user.reached_category_voting_limit?(object.id)
+    end
+
+    def include_votes_exceeded?
+      has_vote_limit
+    end
+
+    def site_vote_limits
+      object.site_vote_limits
+    end
+  end
+
+  class ::Category
+    def site_vote_limits
+      self.custom_fields['site_vote_limits']
+    end
+  end
+
+  class ::Topic
+    def can_vote?
+      SiteSetting.voting_enabled &&
+      (Category.can_vote?(category_id) || subtype === 'voting') &&
+      category.topic_id != id
+    end
+  end
+
+  ::User.class_eval do
+    def vote_count(category_id = nil)
+      user_votes = category_id ? category_votes(category_id) : votes
+
+      if user_votes
+        user_votes.length
+      else
+        0
+      end
+    end
+
+    def alert_low_votes?
+      (vote_limit - vote_count) <= SiteSetting.voting_alert_votes_left
+    end
+
+    def votes
+      [*self.custom_fields["votes"]]
+    end
+
+    def category_votes(category_id)
+      [*self.custom_fields["#{category_id}_votes"]]
+    end
+
+    def votes_archive
+      [*self.custom_fields["votes_archive"]]
+    end
+
+    def reached_voting_limit?
+      vote_count >= vote_limit
+    end
+
+    def vote_limit
+      SiteSetting.send("voting_tl#{self.trust_level}_vote_limit")
+    end
+
+    def category_vote_limit(category_id = nil)
+      return nil if !category_id
+      category = Category.find(category_id)
+      category_limit = category.custom_fields["tl#{self.trust_level}_vote_limit"]
+
+      if category_limit
+        if category.custom_fields["apply_site_vote_limits"]
+          [category_limit, vote_limit].min
+        else
+          category_limit
+        end
+      else
+        nil
+      end
+    end
+
+    def has_category_limit?(category_id)
+      CategoryCustomField.exists?(name: "tl#{self.trust_level}_vote_limit", category_id: category_id)
+    end
+
+    def reached_category_voting_limit?(category_id)
+      vote_count(category_id) >= category_vote_limit(category_id)
+    end
+
+    def add_vote(topic)
+      self.custom_fields["votes"] = votes.dup.push(topic.id)
+      self.custom_fields["#{topic.category.id}_votes"] = category_votes(topic.category.id).dup.push(topic.id)
+      DiscourseEvent.trigger(:vote_added, self, topic)
+    end
+
+    def remove_vote(topic)
+      self.custom_fields["votes"] = votes.dup - [topic.id.to_s]
+      self.custom_fields["#{topic.category.id}_votes"] = category_votes(topic.category.id).dup - [topic.id.to_s]
+      DiscourseEvent.trigger(:vote_removed, self, topic)
+    end
+
+    def remove_archived_vote(topic)
+      self.custom_fields["votes_archive"] = votes_archive.dup.push(topic.id)
+    end
+
+    def add_archived_vote(topic)
+      self.custom_fields["votes_archive"] = votes_archive.dup - [topic.id.to_s]
+    end
+  end
+
+  DiscourseVoting::VotesController.class_eval do
+    def add
+      topic = Topic.find_by(id: params["topic_id"])
+
+      raise Discourse::InvalidAccess if !topic.can_vote?
+      guardian.ensure_can_see!(topic)
+
+      user = current_user
+      voted = false
+      has_category_limit = user.has_category_limit?(topic.category_id)
+      reached_site_limit = user.reached_voting_limit?
+      reached_category_limit = user.reached_category_voting_limit?(topic.category_id) if has_category_limit
+      reached_applicable_limit = has_category_limit ? reached_category_limit : reached_site_limit
+
+      unless reached_applicable_limit
+        user.add_vote(topic)
+        user.save
+        update_vote_count(topic)
+        voted = true
+      end
+
+      vote_limit = has_category_limit ? user.category_vote_limit(topic.category_id) : user.vote_limit
+      user_vote_count = user.vote_count(topic.category_id)
+
+      obj = {
+        user_votes_exceeded: reached_site_limit,
+        user_voted: true,
+        vote_limit: vote_limit,
+        vote_count: topic.custom_fields["vote_count"].to_i,
+        who_voted: who_voted(topic),
+        alert: user.alert_low_votes?,
+        votes_left: [(vote_limit - user_vote_count), 0].max
+      }
+
+      if has_category_limit
+        obj[:category_votes_exceeded] = reached_category_limit
+      end
+
+      render json: obj, status: voted ? 200 : 403
+    end
+
+    def remove
+      topic = Topic.find_by(id: params["topic_id"])
+
+      guardian.ensure_can_see!(topic)
+
+      user = current_user
+      user.remove_vote(topic)
+      user.save
+
+      update_vote_count(topic)
+
+      vote_limit = user.has_category_limit?(topic.category_id) ?
+                   user.category_vote_limit(topic.category_id) :
+                   user.vote_limit
+      obj = {
+        user_votes_exceeded: user.reached_voting_limit?,
+        user_voted: false,
+        vote_limit: vote_limit,
+        vote_count: topic.custom_fields["vote_count"].to_i,
+        who_voted: who_voted(topic),
+        votes_left: [(vote_limit - user.vote_count(topic.category_id)), 0].max
+      }
+
+      if user.has_category_limit?(topic.category_id)
+        obj[:category_votes_exceeded] = user.reached_category_voting_limit?(topic.category_id)
+      end
+
+      render json: obj
+    end
+  end
+end
